16b5e1b49a5483b9b2cf47a266ad2138c626c6be
jesseabrandt <jesse.a.brandt@gmail.com>
2025-04-21 16:29:44 -0400

    clean scrape script and add comments

--- scrape_catalog.R
+++ scrape_catalog.R
@@ -1,32 +1,46 @@
-if (!require("pacman")) install.packages("pacman")
+# This file scrapes the course catalog for 2024-2025.
+#Todo: scrape other years' catalogs
 
-# Load contributed packages with pacman
+# Load packages with pacman
+if (!require("pacman")) install.packages("pacman")
 pacman::p_load(pacman, tidyverse, rvest)
 
-url <- "https://catalog.american.edu/content.php?catoid=22&catoid=22&navoid=4006&filter%5Bitem_type%5D=3&filter%5Bonly_active%5D=1&filter%5B3%5D=1&filter%5Bcpage%5D=7#acalog_template_course_filter"
+
+# Get page one of catalog
+url <- "https://catalog.american.edu/content.php?catoid=22&catoid=22&navoid=4006&filter%5Bitem_type%5D=3&filter%5Bonly_active%5D=1&filter%5B3%5D=1&filter%5Bcpage%5D=1#acalog_template_course_filter"
 page <- read_html(url)
-course_text <- page %>%
-  html_elements(".width a") %>%
+num_pages <- page %>%
+  html_elements("a:nth-child(14)") %>%
   html_text()
-course_href <- page %>%
-  html_elements(".width a") %>%
-  html_attrs("")
 
 
+#Get some attributes of ?
+# course_text <- page %>%
+#  html_elements(".width a") %>%
+# html_text()
+# course_href <- page %>%
+# html_elements(".width a") %>%
+# html_attrs("")
+
+#Writing code for the coming for loop
 course_attributes <- page %>%
   html_elements(".width a") %>%
   html_attrs()
-tbl2 <- tibble(attr = course_attributes) %>%
+tbl <- tibble(attr = course_attributes) %>%
   unnest_wider(attr)#unnest data
 
-#get page of a number i
+# Scrapes every page of classes in catalog
 
-#get course list
-for(i in 1:44){
-  url = paste("https://catalog.american.edu/content.php?catoid=22&catoid=22&navoid=4006&filter%5Bitem_type%5D=3&filter%5Bonly_active%5D=1&filter%5B3%5D=1&filter%5Bcpage%5D=",
-              i, "#acalog_template_course_filter", sep = "")
+
+
+for(i in 2:num_pages){#
+  # 
+  url = paste("https://catalog.american.edu/content.php?catoid=22&catoid=22&navoid=4006&filter%5Bitem_type%5D=3&filter%5Bonly_active%5D=1&filter%5B3%5D=1",
+              "&filter%5Bcpage%5D=",
+              i, sep = "")
+  #"#acalog_template_course_filter" is usually in url
   page = read_html(url)
-
+  
   
   course_attributes <- page %>%
     html_elements(".width a") %>%
@@ -36,16 +50,19 @@ for(i in 1:44){
   tbl <- bind_rows(tbl, tbl2)
   Sys.sleep(1)
 }
-url = paste("https://catalog.american.edu/content.php?catoid=22&catoid=22&navoid=4006&filter%5Bitem_type%5D=3&filter%5Bonly_active%5D=1&filter%5B3%5D=1&filter%5Bcpage%5D=",
-i, "#acalog_template_course_filter", sep = "")
-page = read_html(url)
+
+#practice code for code below
+# url = paste("https://catalog.american.edu/content.php?catoid=22&catoid=22&navoid=4006&filter%5Bitem_type%5D=3&filter%5Bonly_active%5D=1&filter%5B3%5D=1&filter%5Bcpage%5D=",
+#             i, "#acalog_template_course_filter", sep = "")
+# page = read_html(url)
+# 
+# 
+# tbl = tbl %>%
+#   mutate(url = paste("https://catalog.american.edu/",
+#                      href, sep = ""))
 
 
-tbl = tbl %>%
-  mutate(url = paste("https://catalog.american.edu/",
-                     href, sep = ""))
-
-#get course descriptions
+# Get course descriptions
 for(j in 1:nrow(table)){
   page <- read_html(tbl$url[[j]])
   tbl$description[j] <- page %>%
@@ -93,6 +110,7 @@ tbl_full <- left_join(tbl, links_wide, by = "title") %>%
 write_csv(tbl_full, "catalog_scrape.csv")
 #unique(tbl_long$links_href) %>% head(10)
 
+#Now you have some scraped data. If you want more from the pages
 #time for regex
 
 # "([A-Z]*)-(\d*) (.*)(\(\d-*\d*\))"
